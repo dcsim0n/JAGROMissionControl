7cc060466eabbe6b2563640089ec66cca0097c4b
Dana Simmons <dsanalog@gmail.com>
2020-02-22 11:00:58 -0500

    Allow Cross Origin Requests
    Why: frontend control panel will likely have a different origin address
    
    * ...
    
    Installed cors package and added it to the middleware stack
    
    * ...

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 require('dotenv').config();
 
@@ -17,6 +18,7 @@ const app = express();
 const blynkClient = blynk.start();
 mqtt.start();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser())
