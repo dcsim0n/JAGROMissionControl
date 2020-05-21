acf625839b6ad23efbb24f3e2540a83e7afff75e
Dana Simmons <dsanalog@gmail.com>
2020-05-21 10:54:10 -0400

    Test virtual pin read and write actions
    Why: need to make sure that data is sent and received from blynk
    
    *---
    
    How: Injected callback to virtual pin 'read' event handler. The
    callback will be called when the event handler is done.
    Created additional tests for virtual pin 'write' event handlers. This
    will test to make sure that blynk buttons will trigger the correct
    mqtt messages
    
    Had to create a mqqtt mock test harnnes
    
    *---

--- __mocks__/mqtt.js
+++ __mocks__/mqtt.js
@@ -3,11 +3,18 @@
  * Dana Simmons 2020
  * 
  */
+const events = require('events');
+
+class MockMQQTClient extends events.EventEmitter {
+  constructor(){
+    super();
+    this.publish = jest.fn(console.log)
+    this.subscribe = jest.fn(console.log)
+  }
+}
 const MQTTMOCK = {
-  connect: jest.fn( ()=>{
-    return { // the client object
-      publish: jest.fn(console.log)
-    }
+  connect: jest.fn(( )=>{
+    return new MockMQQTClient( ) ;
   })
 }
 
--- __tests__/blynk-test.js
+++ __tests__/blynk-test.js
@@ -5,7 +5,9 @@
 */
 
 const blynk = require('../lib/blynk');
-
+const mqtt = require('mqtt');
+const mqttMock = require('../lib/mqtt');
+const models = require('../models');
 
 describe('Blynk has a client', ()=> {
   test('Blynk has a start method', ()=> {
@@ -20,3 +22,51 @@ describe('Blynk.start returns internal objects', ()=>{
   })
 })
 
+describe('Virtual pins return data when read', ()=>{
+  const {v1, v2} = blynk.start();
+  beforeAll(( )=>{
+    return models.measurement.create({
+      nodemcuId:4,
+      sensorNum:1,
+      value:42
+    })
+  })
+
+  afterAll(( )=>{
+    return models.measurement.destroy({truncate: true});
+  })
+  test('v1.write() gets called',( done )=>{
+
+    const callbackTest = ( )=>{
+      expect(v1.write).toHaveBeenCalled();
+      done();
+    }
+    v1.emit('read', callbackTest );
+  })
+
+  test('v1.write() sends correct data', ( )=>{
+    
+    const callbackTest = ( )=>{
+      expect(v1.write).toHaveBeenCalledWith(42);
+      done();
+    }
+    v1.emit('read', callbackTest );
+  })
+
+})
+
+describe('button pins respond to \'write\' event', ( ) => {
+  
+  const {b1, b2} = blynk.start();
+
+  test('Buttons publish a message for state changes', ( )=>{
+    b1.emit('write',1);
+    expect(mqttMock.client.publish).toHaveBeenCalled();
+  })
+
+  test('Buttons trigger the correct relay message and data', ( ) =>{
+    b1.emit('write', 1);
+    expect(mqttMock.client.publish.mock.calls[1][0]).toBe("jagro/JAGRO1/relay/1");
+    expect(mqttMock.client.publish.mock.calls[1][1]).toBe(1);
+  })
+})
