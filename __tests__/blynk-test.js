6a1933b3a25686065211e0402f723921d6503b9a
Dana Simmons <dsanalog@gmail.com>
2020-05-21 08:28:16 -0400

    Move tests to __tests__ directory and add yarn test
    Why: jest looks for unit tests in the __tests__ directory
    
    *---
    
    How: moved tests to the __tests__ directory and added
    "test" script to package.json file. Now you can run yarn test
    to kick off all the unit tests
    
    *---

--- __tests__/blynk-test.js
+++ __tests__/blynk-test.js
@@ -4,14 +4,13 @@
 *
 */
 
-const jest = require('jest');
 const blynk = require('../lib/blynk');
 
 
 describe('Blynk has a client', ()=> {
   test('Blynk has a start method', ()=> {
     expect(blynk.start).toBeDefined()
-  });)
+  });
 });
 
 describe('Blynk.start returns internal objects', ()=>{
