de51033f4b32d1b00532ccef6920be3cb5eb86da
Dana Simmons <dsanalog@gmail.com>
2020-05-21 11:33:28 -0400

    Add assertion for data passed to client.publish()
    Why: mqtt expects to receive a string or buffer as data,
    the mqtt mock should have the same expectation
    
    *---
    
    How: added assertion for data argument to require all data be a string
    
    *---

--- __mocks__/mqtt.js
+++ __mocks__/mqtt.js
@@ -3,12 +3,16 @@
  * Dana Simmons 2020
  * 
  */
+const assert = require('assert');
 const events = require('events');
 
 class MockMQQTClient extends events.EventEmitter {
   constructor(){
     super();
-    this.publish = jest.fn(console.log)
+    this.publish = jest.fn(( data )=>{
+      assert(typeof(data) === 'string'); 
+      console.log(data)
+    });
     this.subscribe = jest.fn(console.log)
   }
 }
