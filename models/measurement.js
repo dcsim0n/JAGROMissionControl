11f7a398b79934f64e9cd019db76a21ed1e769db
Dana Simmons <dsanalog@gmail.com>
2020-06-06 11:39:20 -0400

    Add indexes to measurements table
    Why: Adding indexes in the testing environment decreased
    cpu usage by 80%
    
    *---
    
    How: add unique index on measurement.id. Added btree index on measurement.time column
    
    *---

--- models/measurement.js
+++ models/measurement.js
@@ -5,7 +5,13 @@ module.exports = (sequelize, DataTypes) => {
     sensorNum: DataTypes.INTEGER,
     value: DataTypes.FLOAT,
     time: DataTypes.DATE
-  }, {});
+  },
+  {
+    indexes: [
+      { name: 'measurements_id_index', unique: true, fields: ['id'] },
+      { name: 'measurements_time_index', fields: ['time'], using: 'BTREE'}
+    ]
+  });
   measurement.associate = function(models) {
     measurement.belongsTo(models.nodemcu);
   };
