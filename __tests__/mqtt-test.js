7105c67ef7bf6cb6a70aa416897d3dfca7698828
Dana Simmons <dsanalog@gmail.com>
2020-06-07 11:53:36 -0400

    Finish implementing mqtt tests
    Why: For better coverage of mqtt functionality
    
    * ...
    
    Added additional tests for sensor data and unrecognized messages
    
    Added setup and tear down to afterAll() and beforeAll() test hooks.
    This will initialize the database to the same state for ALL tests.
    Then cleanup the data after testing is complete.
    
    Debuged some async tests and added expect.assertions() to make sure
    that the correct number of expect() statements are fired.
    
    * ...

--- __tests__/mqtt-test.js
+++ __tests__/mqtt-test.js
@@ -8,8 +8,33 @@ const mqttMock = require('mqtt');
 const mqtt = require('../lib/mqtt'); 
 const models = require('../models');
 
+// do some setup for the database
+beforeAll( ()=>{
+  return models.nodemcu.create({
+    uniqueId:'JAGRO1'
+  })
+  .then( nodemcu =>{
+  
+    return models.relaystatus.bulkCreate([
+          {uniqueId:'JAGRO1', relayNum: 1, status: 0, nodemcuId: nodemcu.id},
+          {uniqueId:'JAGRO1', relayNum: 2, status: 0, nodemcuId: nodemcu.id},
+          {uniqueId:'JAGRO1', relayNum: 3, status: 0, nodemcuId: nodemcu.id},
+          {uniqueId:'JAGRO1', relayNum: 4, status: 0, nodemcuId: nodemcu.id}
+    ]);
+
+  });
+
+});
+
+// clear out the test data
+afterAll( async ()=>{
+  await models.measurement.destroy({truncate:true})
+  await models.nodemcu.destroy({truncate:true})
+  await models.relaystatus.destroy({truncate:true})
+})
 
 describe('JAGRO MQTT can parse mqtt messages', ( )=>{
+
   test('mqtt has a "createJagroMessage" method', ()=>{
     expect(typeof(mqtt.createJagroMessage)).toBe('function');
   })
@@ -41,75 +66,80 @@ describe('JAGRO subscribes when connected', ( )=>{
   // test that messages trigger the correct data in the database
 describe('JAGRO reacts to incoming message', () => {
 
-  afterEach(()=>{
-    models.relaystatus.removeHook('afterUpdate','afterFind');
-    return models.measurement.destroy({truncate:true});
-  });
-
   // relay status should get updated
   test("relay status should get updated", (done)=>{
 
-    // clear out all statuses from previous tests
-    models.relaystatus.destroy({truncate: true})
-    .then( ()=>{
-      //send relay status update
-      mqtt.client.emit('message', 'jagro/JAGRO1/relay/1','1');
-      //create hook to fire after the database is updated
-      models.relaystatus.afterUpdate( r =>{
-        models.relaystatus.findOne({where: {uniqueId: 'JAGRO1'}})
-        .then( relay =>{
-          expect(relay.status).toBe(1);
-          done();
-        });
-
+    //create hook to fire after the database is updated
+    models.relaystatus.afterUpdate( r =>{
+      models.relaystatus.findOne({where: {uniqueId: 'JAGRO1', relayNum: 1}})
+      .then( relay =>{
+        expect(relay.status).toBe(1);
+        done();
       });
-      
+    
     });
+    
+    //send relay status update
+    mqtt.client.emit('message', 'jagro/JAGRO1/relay/1','1');
 
   });
   
   // sync message should be processed
   test("sync messages should be return a response", (done)=>{
 
-    //add some relays
-    models.relaystatus.bulkCreate([
-      {uniqueId:'JAGRO1', relayNum: 2, status: 0, nodemcuId:1},
-      {uniqueId:'JAGRO1', relayNum: 3, status: 0, nodemcuId:1},
-      {uniqueId:'JAGRO1', relayNum: 4, status: 0, nodemcuId:1}
-    ])
-    .then(()=>{
-      mqtt.client.emit('message', 'jagro/JAGRO1/sync','');
-      // not pretty but since the getRelaystatuses() is async, need to wait
-      // for it to return and publish to be called
-      setTimeout(()=>{
-          expect(mqtt.client.publish).toHaveBeenCalledTimes(4);
-          done();
-      }, 2000);
-
-    });
+    mqtt.client.emit('message', 'jagro/JAGRO1/sync','');
+    // not pretty but since the getRelaystatuses() is async, need to wait
+    // for it to return and publish to be called
+    setTimeout(()=>{
+        expect(mqtt.client.publish).toHaveBeenCalledTimes(4);
+        done();
+    }, 2000);
 
   });
 
   // sensor data should be written
-
   test("sensor data should be recorded", (done)=>{
+
+    expect.assertions(1)
+
     // emit sensor message
     for(let i = 1; i < 6; i++){
-      mqtt.client.emit('message', `jagro/JAGRO1/sensor/${i}`,toString(i));
+      mqtt.client.emit('message', `jagro/JAGRO1/sensor/${i}`,String(i));
     }
 
     setTimeout(()=>{
       models.measurement.count()
       .then( c => {
+        // make sure that the count of records is correct
         expect(c).toBe(5);
-        done()
       })
+      .finally( ()=> done());
+     
     },2000)
-    // make sure that the count of records is correct
+
   })
 
-  test("unrecognized messages should not cause errors", ()=>{
+  test("unrecognized messages should not cause errors", (done)=>{
+    //unknown messages should be accepted without error
+    //unknown messages should have no side effects
 
+    expect.assertions(6);
+
+    mqtt.client.emit('message', 'test/123','no data');
+
+    models.measurement.count()
+    .then (c => {
+      expect(c).toBe(5);
+      return models.relaystatus.findAll();
+    })
+    .then( relays =>{
+      expect(relays.length).toBe(4);
+      expect(relays[0].status).toBe(1);
+      expect(relays[1].status).toBe(0);
+      expect(relays[2].status).toBe(0);
+      expect(relays[3].status).toBe(0);
+    })
+    .finally(()=>done())
   })
 })
 
